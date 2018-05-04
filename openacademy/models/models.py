547b7ae66b1da2e3f0a2c527b8f581b9055a2b3b
santonyytyp <jmartinez@typrefrigeracion.com>
2018-05-04 12:46:25 -0700

    [REF] constraints

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions
 
 def get_uid(self, *a):
 	return self.env.uid
@@ -16,6 +16,36 @@ class Course(models.Model):
 #	responsible_id = fields.Many2one('res.users', string="Responsible", index=True, ondelete='set null', default=lambda self, *a: self.env.uid)
 	responsible_id = fields.Many2one('res.users', string="Responsible", index=True, ondelete='set null', default=get_uid)
 	session_ids = fields.One2many('openacademy.session','course_id')
+
+	_sql_constraints = [
+		('name_description_check',
+		 'CHECK( name != description)',
+		 "The title of the course should not be the description"
+		),
+		('name_unique',
+		 'UNIQUE(name)',
+		 "The course title must be unique"
+		),
+	]
+
+#	def copy(self, default=None):
+#		if default is None:
+#			default = {}
+#		default['name'] = self.name + ' otro'
+#		return super(Course, self).copy(default)
+
+	def copy(self, default=None):
+		if default is None:
+			default = {}
+		copied_count = self.search_count([('name', 'ilike', 'Copy of %s%%' % (self.name))])
+		if not copied_count:
+			new_name = "Copy of %s" % (self.name)
+		else:
+			new_name = "Copy of %s (%s)" % (self.name, copied_count)
+		default['name'] = new_name
+		return super(Course, self).copy(default)
+
+
 #
 #     @api.depends('value')
 #     def _value_pc(self):
@@ -49,3 +79,28 @@ class Session(models.Model):
 #				record.taken_seats = 0
 #			else:
 #				record.taken_seats = 100.0 * len(record.attendee_ids) / record.seats
+	@api.onchange('seats','attendee_ids')
+	def _verify_valid_seats(self):
+		if self.filtered(lambda r: r.seats < 0):
+			self.active = False
+			return {
+				'warning': {
+					'title': "Incorrect 'seats' value",
+					'message': "The number of available seats may not be negative",
+				}
+			}
+		if self.seats < len(self.attendee_ids):
+			self.active = False
+			return {
+				'warning': {
+					'title': "Too many attendees",
+					'message': "Increase seats or remove excess attendees",
+				}
+			}
+		self.active = True
+
+	@api.constrains('instructor_id','attendee_ids')
+	def _check_instructor_not_in_attendees(self):
+		for record in self.filtered('instructor_id'):
+			if record.instructor_id in record.attendee_ids:
+				raise exceptions.ValidationError("A session's instructor can't be an attendee")
