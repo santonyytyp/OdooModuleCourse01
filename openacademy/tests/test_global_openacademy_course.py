8e86b569b39f35858ea0009c6b2ef72722d618f8
santonyytyp <jmartinez@typrefrigeracion.com>
2018-06-26 16:51:47 -0700

    [REF] openacademy: Add duplicate test to openacademy course

--- openacademy/tests/test_global_openacademy_course.py
+++ openacademy/tests/test_global_openacademy_course.py
@@ -18,6 +18,7 @@ class GlobalTestOpenAcademyCourse(TransactionCase):
 
 	#Method of class that don't is test.
 	def create_course(self, course_name, course_description, course_responsible_id):
+		# Create a course with parameters received
 		course_id = self.course.create({
 			'name': course_name,
 			'description': course_description,
@@ -26,14 +27,41 @@ class GlobalTestOpenAcademyCourse(TransactionCase):
 		return course_id
 
 	#Method of test startswith 'def test_*(self):'
+
+	# mute the error odoo.sql_db to avoid it in log
 	@mute_logger('odoo.sql_db')
-	def test_01_same_name_description(self):
+	def test_10_same_name_description(self):
 		'''
 		Test create a course with same name and description.
 		To test contraint of name different to description.
 		'''
+		# Error raised expected with message expected
 		with self.assertRaisesRegexp(
 			IntegrityError,
 			'new row for relation "openacademy_course" violates check constraint "openacademy_course_name_description_check"'
 			):
+			# create a course with same name and description to raise error
 			self.create_course('test', 'test', None)
+
+	@mute_logger('odoo.sql_db')
+	def test_20_create_two_courses_same_name(self):
+		'''
+		Test to create two courses with same name.
+		To raise constraint of unique name.
+		'''
+		new_id = self.create_course('test1', 'test_description', None)
+		print ("new_id", new_id)
+		with self.assertRaisesRegexp(
+			IntegrityError,
+			'duplicate key value violates unique constraint "openacademy_course_name_unique"'
+			):
+			new_id2 = self.create_course('test1', 'test_description', None)
+			print ("new_id2", new_id2)
+
+	def test_15_duplicate_course(self):
+		'''
+		Test to duplicate a course and check work fine!
+		'''
+		course = self.env.ref('openacademy.course0')
+		course_id = course.copy()
+		print ('course_id', course_id)
